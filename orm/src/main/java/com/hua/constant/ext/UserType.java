2f1cfaedc614c5db6d0047b0d8d399c68d123d6f
qianye.zheng <dearzqy@163.com>
2018-11-30 21:42:48 +0800

    no message

--- orm/src/main/java/com/hua/constant/ext/UserType.java
+++ orm/src/main/java/com/hua/constant/ext/UserType.java
@@ -13,4 +13,36 @@ package com.hua.constant.ext;
  */
 public enum UserType {
 
+    SHOP_MANAGER(1, "店长"),
+
+    SHOPER(2, "店员"),
+
+    GROUP(3, "集团用户"),
+
+    INVESTOR(4, "投资人");
+	
+	private Integer value;
+
+    private String remark;
+    
+    private UserType(final Integer value, final String remark) {
+        this.value = value;
+        this.remark = remark;
+    }
+
+	/**
+	* @return the value
+	*/
+	public Integer getValue()
+	{
+		return value;
+	}
+
+	/**
+	* @return the remark
+	*/
+	public String getRemark()
+	{
+		return remark;
+	}
 }
